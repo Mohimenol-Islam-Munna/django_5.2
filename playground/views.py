d3c802de814c52198576e3c377efdf92f5525ba5
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2025-10-21 03:11:50 +0600

    django debug toolbar

--- playground/views.py
+++ playground/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def playground_hello_view(request):
-    return HttpResponse("Hello from the playground app!")
+    # return HttpResponse("Hello from the playground app!")
+    a = 10
+    b = 20
+    return render(request, "playground/hello.html", {"name": "Full Stack Developer"})
